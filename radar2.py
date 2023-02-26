e32edf6a65fef44f6e0618504e990078945ef82d
Mikhail Yudin <fagci.nsk@gmail.com>
2023-02-26 18:45:56 +0700

    refactor

--- radar2.py
+++ radar2.py
@@ -20,74 +20,80 @@ from ipaddress import IPv4Network
 
 TIMEOUT = 2
 
-if len(argv) < 2:
-    print(f'Usage: {argv[0]} <interface>')
-    exit(255)
-
-iface = argv[1]
-
-s = socket(AF_PACKET, SOCK_RAW, htons(0x0806))
-s.settimeout(0.25)
-s.bind((iface, 0))
 
 
-targets = {}
+class ARPScan(socket):
+    def __init__(self, iface):
+        super().__init__(AF_PACKET, SOCK_RAW, htons(0x0806))
+        self.iface = iface
+        self.settimeout(0.25)
+        self.bind((iface, 0))
+        self.targets = {}
+        self.ipsi, self.ipsmask = self.ifipmaski()
+        self.macs = self.getsockname()[4]
+        self.macd = b'\xff\xff\xff\xff\xff\xff'
 
-def ifipmaski(iface):
-    with socket(AF_INET, SOCK_DGRAM) as so:
-        ifacep = pack('256s', iface.encode('utf_8'))
-        return (
-            ioctl(so.fileno(), 0x8915, ifacep)[20:24],
-            ioctl(so.fileno(), 0x891b, ifacep)[20:24]
-        )
+    def ifipmaski(self):
+        with socket(AF_INET, SOCK_DGRAM) as so:
+            ifacep = pack('256s', self.iface.encode('utf_8'))
+            return (
+                ioctl(so.fileno(), 0x8915, ifacep)[20:24],
+                ioctl(so.fileno(), 0x891b, ifacep)[20:24]
+            )
 
-ipsi, ipsmask = ifipmaski(iface)
-macs = s.getsockname()[4]
-macd = b'\xff\xff\xff\xff\xff\xff'
 
-def scan(ipd):
-    ipdi = inet_aton(ipd)
+    def ping(self, ip):
+        ipdi = inet_aton(ip)
 
-    eth = pack('!6s6sH', macd, macs, 0x0806)
-    arp = pack('!HHBBH6s4s6s4s', 1, 0x0800, 6, 4, 1, macs, ipsi, macd, ipdi)
-    packet = eth + arp
-    s.send(packet)
-    targets[ipdi] = time() + TIMEOUT
+        eth = pack('!6s6sH', self.macd, self.macs, 0x0806)
+        arp = pack('!HHBBH6s4s6s4s', 1, 0x0800, 6, 4, 1, self.macs, self.ipsi, self.macd, ipdi)
+        packet = eth + arp
+        self.send(packet)
+        self.targets[ipdi] = time() + TIMEOUT
 
-def recv():
-    te = time() + TIMEOUT * 2
-    while time() < te:
-        try:
-            packet, _ = s.recvfrom(2048)
-        except:
-            continue
-        # eth = unpack('!6s6s2s', packet[0:14])
-        arp = unpack('2s2s1s1s2s6s4s6s4s', packet[14:42])
-        macd = '%x:%x:%x:%x:%x:%x' % unpack('BBBBBB', arp[5])
-        ipdi = arp[6]
-        timeout = targets.get(ipdi)
-        if timeout and time() <= timeout:
-            ipd = inet_ntoa(ipdi)
+    def recv(self):
+        te = time() + TIMEOUT
+        while time() < te:
             try:
-                host, *_ = gethostbyaddr(ipd)
+                packet, _ = self.recvfrom(2048)
             except:
-                host = ''
-            print(ipd, macd, host)
+                continue
+            # eth = unpack('!6s6s2s', packet[0:14])
+            arp = unpack('2s2s1s1s2s6s4s6s4s', packet[14:42])
+            macd = '%x:%x:%x:%x:%x:%x' % unpack('BBBBBB', arp[5])
+            ipdi = arp[6]
+            timeout = self.targets.get(ipdi)
+            if timeout and time() <= timeout:
+                ipd = inet_ntoa(ipdi)
+                try:
+                    host, *_ = gethostbyaddr(ipd)
+                except:
+                    host = ''
+                print(ipd, macd, host)
 
-def scan_range():
-    ips = inet_ntoa(ipsi)
-    ipsp = ips.split('.')
-    ipsp[3] = '0'
-    n = '.'.join(ipsp)
-    net = IPv4Network(f'{n}/{inet_ntoa(ipsmask)}', False)
-    for ip in net:
-        scan(str(ip))
-        sleep(0.01)
+    def scan_range(self):
+        ips = inet_ntoa(self.ipsi)
+        ipsp = ips.split('.')
+        ipsp[3] = '0'
+        net = '.'.join(ipsp)
+        mask = inet_ntoa(self.ipsmask)
+        for ip in IPv4Network(f'{net}/{mask}', False):
+            self.ping(str(ip))
+            sleep(0.01)
 
-rt = Thread(target=recv)
-st = Thread(target=scan_range)
+    def scan(self):
+        rt = Thread(target=self.recv)
+        st = Thread(target=self.scan_range)
 
-rt.start()
-st.start()
+        rt.start()
+        st.start()
 
-rt.join()
+        rt.join()
+
+if __name__ == '__main__':
+    if len(argv) < 2:
+        print(f'Usage: {argv[0]} <interface>')
+        exit(255)
+
+    scanner = ARPScan(argv[1])
+    scanner.scan()
